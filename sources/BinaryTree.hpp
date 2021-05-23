82d9007db7a1822eb2b1cd53a5dd01f67655f212
RavidSaadia <ravids1233@gmail.com>
2021-05-23 18:38:00 +0300

    iterator struct

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -1,7 +1,7 @@
 //
 // Created by ravid on 23/05/2021.
 //
-
+#pragma once
 #ifndef UNTITLED1_BINARYTREE_H
 #define UNTITLED1_BINARYTREE_H
 
@@ -12,6 +12,7 @@ namespace ariel {
 
     template<typename T>
     class BinaryTree {
+
     private:
         struct Node {
             T _value;
@@ -27,14 +28,16 @@ namespace ariel {
 
         };
 
-        size_t _size;
         Node *_root;
+        size_t _size;
+        size_t _depth;
+
+        BinaryTree(const BinaryTree &binaryTree) {};
 
 
     public:
-        BinaryTree(const BinaryTree &binaryTree) {};
 
-        BinaryTree() : _root(nullptr), _size(0) {};
+        BinaryTree() : _root(nullptr), _size(0), _depth(0) {};
 
         ~BinaryTree() {
             delete _root;
@@ -51,44 +54,58 @@ namespace ariel {
         struct Iterator {
         private:
             Node *_cur_node;
-            size_t i;
-            size_t type;
+            size_t _i;
+            size_t _type; // 0 = pre, 1 = in, 2 = post
         public:
-            T& operator*(){
-                return _cur_node->_value;
-            }
-            T& operator->(){ // *T?
-                return &(_cur_node->_value);
-            }
-            bool operator==(const Iterator &iterator)const{
-                return iterator == _cur_node;
-            };
+            Iterator(Node *node, int type);
 
+            T &operator*() { return _cur_node->_value; }
 
-            bool operator!=(const Iterator &iterator){
-                return iterator != _cur_node;
-            };
+            T *operator->() { return &(_cur_node->_value); }
 
-
-            Iterator& operator++();//++i
-
+            Iterator &operator++();//++i
 
             const Iterator operator++(int);//i++
 
+            bool operator==(const Iterator &iterator) const;
+
+            bool operator!=(const Iterator &iterator) const;
 
         };//Iterator
 
-        Iterator begin_preorder();
+        Iterator begin_preorder() { return Iterator{_root, 0}; };
+
+        Iterator end_preorder() { return Iterator{nullptr, 0}; };
+
+        Iterator begin_inorder() { return Iterator{_root, 1}; };
+
+        Iterator end_inorder() { return Iterator{nullptr, 1}; };
+
+        Iterator begin_postorder() { return Iterator{_root, 2}; };
+
+        Iterator end_postorder() { return Iterator{nullptr, 2}; };
+
+        Iterator begin() { return begin_inorder(); }
+
+        Iterator end() { return end_inorder(); }
     };
 
     template<typename T>
     BinaryTree<T> &BinaryTree<T>::add_root(T value) {
+        if (_root == nullptr){
+            _root = new Node{value};
+            ++_size;
+        }else{
+            _root->_value =value;
+        }
 
         return *this;
     }
 
     template<typename T>
     BinaryTree<T> &BinaryTree<T>::add_left(T exist_value, T added_value) {
+
+
         return *this;
     }
 
@@ -103,21 +120,34 @@ namespace ariel {
         return os;
     }
 
-    template<typename T>
-    BinaryTree::Iterator BinaryTree<T>::begin_preorder() {
-        return BinaryTree::Iterator();
-    }
 
     template<typename T>
-     typename BinaryTree<T>::Iterator &BinaryTree<T>::Iterator::operator++() {
+    typename BinaryTree<T>::Iterator &BinaryTree<T>::Iterator::operator++() {
 
         return *this;
     }
+
     template<typename T>
     const typename BinaryTree<T>::Iterator BinaryTree<T>::Iterator::operator++(int) {
 
         return *this;
     }
+
+    template<typename T>
+    BinaryTree<T>::Iterator::Iterator(BinaryTree::Node *node, int type):_cur_node(node), _type(type) {
+        //TODO
+    }
+
+    template<typename T>
+    bool BinaryTree<T>::Iterator::operator==(const BinaryTree<T>::Iterator &iterator) const {
+        return iterator == _cur_node;
+    }
+
+    template<typename T>
+    bool BinaryTree<T>::Iterator::operator!=(const BinaryTree<T>::Iterator &iterator) const {
+        return iterator != _cur_node;
+    }
+
 
 }
 #endif //UNTITLED1_BINARYTREE_H
