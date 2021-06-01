b103ac5458dd656819519c5c9929ae8812e8a11d
RavidSaadia <ravids1233@gmail.com>
2021-06-01 11:07:12 +0300

    tests

--- Test.cpp
+++ Test.cpp
@@ -39,12 +39,6 @@ TEST_CASE ("Test Basic Tree ")
       ********************************************
   */
 
-
-
-
-
-
-
             SUBCASE("printing") {
         cout << tree_of_ints << endl;
     }
@@ -52,69 +46,112 @@ TEST_CASE ("Test Basic Tree ")
 
 }
 
-TEST_CASE ("add root") {
-    BinaryTree<int> int_tree;
-    for (int i = 0; i < 10; ++i) {
-        int root = rand();
-                CHECK_NOTHROW(int_tree.add_root(root));
-                CHECK_EQ(*int_tree.begin_inorder(), root);
-                CHECK_EQ(*int_tree.begin_postorder(), root);
-                CHECK_EQ(*int_tree.begin_preorder(), root);
+TEST_CASE ("int tree") {
+            SUBCASE ("add root") {
+        BinaryTree<int> int_tree;
+        for (int i = 0; i < 10; ++i) {
+            int root = rand();
+                    CHECK_NOTHROW(int_tree.add_root(root));
+                    CHECK_EQ(*int_tree.begin_inorder(), root);
+                    CHECK_EQ(*int_tree.begin_postorder(), root);
+                    CHECK_EQ(*int_tree.begin_preorder(), root);
 
+        }
     }
-}
 
-TEST_CASE ("add right") {
-    BinaryTree<int> int_tree;
-    for (int i = 0; i < 10; ++i) {
-        int root = rand();
-        int right = rand();
-                CHECK_NOTHROW(int_tree.add_root(root));
-                CHECK_NOTHROW(int_tree.add_left(root, right));
-                CHECK_EQ(*(int_tree.begin_inorder()), right);
-                CHECK_EQ(*int_tree.begin_postorder(), right);
-                CHECK_EQ(*int_tree.begin_preorder(), root);
+            SUBCASE ("add right") {
+        BinaryTree<int> t1;
+        for (int i = 0; i < 10; ++i) {
+            int root = rand();
+            int right = rand();
+                    CHECK_NOTHROW(t1.add_root(root));
+                    CHECK_NOTHROW(t1.add_left(root, right));
+                    CHECK_EQ(*(t1.begin_inorder()), right);
+                    CHECK_EQ(*t1.begin_postorder(), right);
+                    CHECK_EQ(*t1.begin_preorder(), root);
 
-    }
-    //    //   tree with only lefts sun`s from 0 to 10
-    int_tree.add_root(0);
-    for (int i = 0; i < 10; ++i) {
-        int_tree.add_right(i, i + 1);
-    }
-    int i = 10; // compere int
+        }
+        //    //   tree with only right sun`s from 0 to 10
+        // root-> 0
+        //         \
+    //          1-2-3-4-5-6-7-8-9-10
 
-            SUBCASE("post") {
-        for (auto it = int_tree.begin_postorder(); it != int_tree.end_postorder(); ++it) {
+        BinaryTree<int> t2;
+        t2.add_root(0);
+        for (int i = 0; i < 10; ++i) {
+            t2.add_right(i, i + 1);
+        }
+        int i = 10; // compere int
+        for (auto it = t2.begin_postorder(); it != t2.end_postorder(); ++it) {
                     CHECK_EQ(*it, i--);
         }
 
-    }
-            SUBCASE("pre") {
-
-        i = 10;
-        for (auto it = int_tree.begin_preorder(); it != int_tree.end_preorder(); ++it) {
-                    CHECK_EQ(*it, i--);
-        }
-    }
-            SUBCASE("in") {
-
+        //post
         i = 0;
-        for (auto it = int_tree.begin_inorder(); it != int_tree.end_inorder(); ++it) {
+        for (auto it = t2.begin_preorder(); it != t2.end_preorder(); ++it) {
                     CHECK_EQ(*it, i++);
         }
-    }
-}
 
-TEST_CASE ("add left") {
-    BinaryTree<int> int_tree;
-    for (int i = 0; i < 10; ++i) {
-        int root = rand();
-        int left = rand();
-                CHECK_NOTHROW(int_tree.add_root(root));
-                CHECK_NOTHROW(int_tree.add_left(root, left));
-                CHECK_EQ(*(int_tree.begin_inorder()), left);
-                CHECK_EQ(*int_tree.begin_postorder(), left);
-                CHECK_EQ(*int_tree.begin_preorder(), root);
+        //in
+        i = 0;
+        for (auto it = t2.begin_inorder(); it != t2.end_inorder(); ++it) {
+                    CHECK_EQ(*it, i++);
+        }
+
+    }
+
+            SUBCASE ("add left") {
+        BinaryTree<int> t1;
+        for (int i = 0; i < 10; ++i) {
+            int root = rand();
+            int left = rand();
+                    CHECK_NOTHROW(t1.add_root(root));
+                    CHECK_NOTHROW(t1.add_left(root, left));
+                    CHECK_EQ(*(t1.begin_inorder()), left);
+                    CHECK_EQ(*t1.begin_postorder(), left);
+                    CHECK_EQ(*t1.begin_preorder(), root);
+
+        }
+        //    //   tree with only left sun`s from 0 to 10
+        //               root-> 0
+        //                     /
+        // 10-9-8-7-6-5-4-3-2-1
+
+        int i = 10; // compere int
+        BinaryTree<int> t2;
+        t2.add_root(0);
+        for (int i = 0; i < 10; ++i) {
+            t2.add_left(i, i + 1);
+        }
+//post
+        for (auto it = t2.begin_postorder(); it != t2.end_postorder(); ++it) {
+                    CHECK_EQ(*it, i--);
+        }
+
+//pre
+        i = 0;
+        for (auto it = t2.begin_preorder(); it != t2.end_preorder(); ++it) {
+                    CHECK_EQ(*it, i++);
+        }
+//in
+        i = 10;
+        for (auto it = t2.begin_inorder(); it != t2.end_inorder(); ++it) {
+                    CHECK_EQ(*it, i--);
+        }
+    }
+
+            SUBCASE("pre") {
+BinaryTree<int> t1;
+
+    }
+
+            SUBCASE("post") {
+
+
+    }
+
+            SUBCASE("in") {
+
 
     }
 }
